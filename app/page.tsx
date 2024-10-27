91b4e3a456c47b2f769f41920160c0145bedafc4
Carlos Rincon <carlosjrinconavila@gmail.com>
2024-10-27 07:00:36 -0500

    feat: improved logind

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,17 @@ export default function Home() {
 
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <div className="flex space-x-8">
-                <div className="flex flex-col items-center p-8 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200 cursor-pointer w-48 h-48 hover:bg-gray-700"
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div className="flex flex-col items-center p-8 bg-gradient-to-br rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200 cursor-pointer w-48 h-48 from-teal-500 to bg-cyan-700"
                     onClick={() => selectUser(StorageUsers.Carlos)}>
                     <div className="text-2xl font-bold text-white mb-4">Carlos</div>
-                    <FaMale size={"4em"}/>
+                    <FaMale size={"4em"} className="text-white"/>
                 </div>
 
-                <div className="flex flex-col items-center p-8 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200 cursor-pointer w-48 h-48 hover:bg-gray-700"
+                <div className="flex flex-col items-center p-8 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200 cursor-pointer w-48 h-48 bg-gradient-to-br from-amber-500 to bg-rose-500"
                     onClick={() => selectUser(StorageUsers.Maye)}>
                     <div className="text-2xl font-bold text-white mb-4">Maye</div>
-                    <FaFemale size={"4em"}/>
+                    <FaFemale size={"4em"} className="text-white"/>
                 </div>
             </div>
         </div>
