626b89b266daf221952f3608f2f13be80ea08d88
Carlos Rincon <carlosjrinconavila@gmail.com>
2024-10-29 19:29:10 -0500

    feat: added menu responsive

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -55,7 +55,7 @@ export default function Layout({
                         size="lg"
                         href={route.path}
                     >
-                    {route.label}
+                        {route.label}
                     </Link>
                 </NavbarMenuItem>
             )
@@ -76,21 +76,21 @@ export default function Layout({
                 isBordered 
                 onMenuOpenChange={setIsMenuOpen}
                 classNames={{
-                item: [
-                    "flex",
-                    "relative",
-                    "h-full",
-                    "items-center",
-                    "data-[active=true]:after:content-['']",
-                    "data-[active=true]:after:absolute",
-                    "data-[active=true]:after:bottom-0",
-                    "data-[active=true]:after:left-0",
-                    "data-[active=true]:after:right-0",
-                    "data-[active=true]:after:h-[2px]",
-                    "data-[active=true]:after:rounded-[2px]",
-                    "data-[active=true]:after:bg-primary",
-                ],
-            }}> 
+                    item: [
+                        "flex",
+                        "relative",
+                        "h-full",
+                        "items-center",
+                        "data-[active=true]:after:content-['']",
+                        "data-[active=true]:after:absolute",
+                        "data-[active=true]:after:bottom-0",
+                        "data-[active=true]:after:left-0",
+                        "data-[active=true]:after:right-0",
+                        "data-[active=true]:after:h-[2px]",
+                        "data-[active=true]:after:rounded-[2px]",
+                        "data-[active=true]:after:bg-primary",
+                    ],
+                }}> 
                 <NavbarContent>
                     <NavbarMenuToggle
                         aria-label={isMenuOpen ? "Close menu" : "Open menu"}
