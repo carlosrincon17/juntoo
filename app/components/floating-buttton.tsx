c5914155963da2a7d7780b768181fe6af7571e3d
Carlos Rincon <carlosjrinconavila@gmail.com>
2025-03-10 09:48:34 -0500

    fix: improved savings module

--- app/components/floating-buttton.tsx
+++ app/components/floating-buttton.tsx
@@ -9,17 +9,17 @@ interface FloatingAddButtonProps {
 }
 
 export function FloatingAddButton({ onClick, label = "Add Savings" }: FloatingAddButtonProps) {
-  return (
-    <Button
-      onClick={onClick}
-      className="fixed bottom-6 right-6 h-14 w-14 sm:h-16 sm:w-16 rounded-full z-50 opacity-100"
-      aria-label={label}
-      color="primary"
-      isIconOnly 
-    >
-      <FaPlus className="h-4 w-4" />
-      <span className="sr-only">{label}</span>
-    </Button>
-  )
+    return (
+        <Button
+            onClick={onClick}
+            className="fixed bottom-6 right-6 h-14 w-14 sm:h-16 sm:w-16 rounded-full z-50 opacity-100"
+            aria-label={label}
+            color="primary"
+            isIconOnly 
+        >
+            <FaPlus className="h-4 w-4" />
+            <span className="sr-only">{label}</span>
+        </Button>
+    )
 }
 
