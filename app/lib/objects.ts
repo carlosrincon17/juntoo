1d7416c5e7a6268124eb200de39bd01c2ddb4817
Carlos Rincon <carlosjrinconavila@gmail.com>
2025-05-01 00:48:20 -0500

    feat: improved dashboard and saving income jobs

--- app/lib/objects.ts
+++ app/lib/objects.ts
@@ -4,14 +4,3 @@ export function getAttribute<T extends object>(obj: T, key: string): T[keyof T] 
     }
     return undefined;
 }
-
-export function groupBy<T, K extends keyof string | number | symbol>(array: T[], keyGetter: (item: T) => K): Record<K, T[]> {
-    return array.reduce((result, currentItem) => {
-        const key = keyGetter(currentItem);
-        if (!result[key]) {
-            result[key] = [];
-        }
-        result[key].push(currentItem);
-        return result;
-    }, {} as Record<K, T[]>);
-}
