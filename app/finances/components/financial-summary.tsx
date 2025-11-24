07fc878cf3db2fc9645f4992a19e16b0b881045c
Carlos Rincon <carlosjrinconavila@gmail.com>
2025-11-24 18:07:15 -0500

    feat: added transactions to dashboard list

--- app/finances/components/financial-summary.tsx
+++ app/finances/components/financial-summary.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import { getFinancialMetrics } from "../actions/financial-metrics";
 import FinancialVariation from "./financial-variaton";
 import { CustomLoading } from "@/app/components/customLoading";
+import TransactionsList from "./transactions-list";
 
 const emptyFinancialMetrics: FinancialMetrics = {
     expenses: {
@@ -51,51 +52,56 @@ export default function FinancialSummary() {
             <CustomLoading />
         ) : (
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <Card className="shadow-md bg-gradient-to-r from-[#8b5cf6] via-[#9333ea] to-[#a78bfa] xl:col-span-1">
-                    <div className="p-4 relative overflow-hidden h-full">
-                        <div className="absolute top-0 right-0 w-64 h-64 bg-gradient-to-br from-white/10 to-transparent rounded-full -translate-y-32 translate-x-32"></div>
-                        <div className="absolute bottom-0 left-0 w-32 h-32 bg-gradient-to-tr from-white/5 to-transparent rounded-full translate-y-16 -translate-x-16"></div>
+                <div className="col-span-1 gap-6 grid grid-cols-1">
+                    <Card className="shadow-md bg-gradient-to-r from-[#8b5cf6] via-[#9333ea] to-[#a78bfa]">
+                        <div className="p-4 relative overflow-hidden h-full">
+                            <div className="absolute top-0 right-0 w-64 h-64 bg-gradient-to-br from-white/10 to-transparent rounded-full -translate-y-32 translate-x-32"></div>
+                            <div className="absolute bottom-0 left-0 w-32 h-32 bg-gradient-to-tr from-white/5 to-transparent rounded-full translate-y-16 -translate-x-16"></div>
 
-                        <div className="flex items-center justify-between mb-1 relative">
-                            <div className="flex items-center gap-1">
-                                <p className="text-md font-light text-white/70">Ahorro</p>
-                            </div>
-                        </div>
-
-                        <div className="flex items-end">
-                            <p className="text-3xl font-extralight text-white">{formatCurrency(financialMetrics.savings.total || 0)}</p>
-                        </div>
-
-                        <div className="flex items-center gap-1 relative">
-                            <FinancialVariation
-                                variationPercentage={financialMetrics.savings.variationPercentage}
-                                amount={financialMetrics.savings.variationTotal}
-                            />
-                        </div>
-
-                        <div className="mt-4 pt-4 border-t border-white/10 block">
-                            <div className="grid gap-4 md:flex justify-between items-center">
-                                <div>
-                                    <p className="text-md font-light text-white/70">Gastos</p>
-                                    <p className="text-2xl font-extralight text-white">{formatCurrency(financialMetrics.expenses.total || 0)}</p>
-                                    <FinancialVariation
-                                        variationPercentage={financialMetrics.expenses.variationPercentage}
-                                        amount={financialMetrics.expenses.variationTotal}
-                                    />
-                                </div>
-                                <div className="mt-4 md:mt-0">
-                                    <p className="text-md font-light text-white/70">Ingresos</p>
-                                    <p className="text-2xl font-extralight text-white">{formatCurrency(financialMetrics.investmentIncome.total || 0)}</p>
-                                    <FinancialVariation
-                                        variationPercentage={financialMetrics.investmentIncome.variationPercentage}
-                                        amount={financialMetrics.investmentIncome.variationTotal}
-                                    />
+                            <div className="flex items-center justify-between mb-1 relative">
+                                <div className="flex items-center gap-1">
+                                    <p className="text-md font-light text-white/70">Ahorro</p>
                                 </div>
                             </div>
+
+                            <div className="flex items-end">
+                                <p className="text-3xl font-extralight text-white">{formatCurrency(financialMetrics.savings.total || 0)}</p>
+                            </div>
+
+                            <div className="flex items-center gap-1 relative">
+                                <FinancialVariation
+                                    variationPercentage={financialMetrics.savings.variationPercentage}
+                                    amount={financialMetrics.savings.variationTotal}
+                                />
+                            </div>
+
+                            <div className="mt-4 pt-4 border-t border-white/10 block">
+                                <div className="grid gap-4 md:flex justify-between items-center">
+                                    <div>
+                                        <p className="text-md font-light text-white/70">Gastos</p>
+                                        <p className="text-2xl font-extralight text-white">{formatCurrency(financialMetrics.expenses.total || 0)}</p>
+                                        <FinancialVariation
+                                            variationPercentage={financialMetrics.expenses.variationPercentage}
+                                            amount={financialMetrics.expenses.variationTotal}
+                                        />
+                                    </div>
+                                    <div className="mt-4 md:mt-0">
+                                        <p className="text-md font-light text-white/70">Ingresos</p>
+                                        <p className="text-2xl font-extralight text-white">{formatCurrency(financialMetrics.investmentIncome.total || 0)}</p>
+                                        <FinancialVariation
+                                            variationPercentage={financialMetrics.investmentIncome.variationPercentage}
+                                            amount={financialMetrics.investmentIncome.variationTotal}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </Card>
-                <FinancialGoals />
+                    </Card>
+                    <FinancialGoals />
+                </div>
+                <div className="col-span-1 md:col-span-2">
+                    <TransactionsList />
+                </div>
             </div>
         )
     )
--- app/finances/components/transactions-list.tsx
+++ app/finances/components/transactions-list.tsx
@@ -16,12 +16,12 @@ export default function TransactionsList() {
     const [transactionTypeSelected, setTransactionTypeSelected] = useState<TransactionType>(TransactionType.Outcome);
     const [currentTransactionsPage, setCurrentTransactionsPage] = useState<number>(1);
     const [loading, setLoading] = useState(true);
-    const {isOpen: isDeleteModalOpen, onOpen: onDeleteModalOpen, onOpenChange: onDeleteModalChange} = useDisclosure();
+    const { isOpen: isDeleteModalOpen, onOpen: onDeleteModalOpen, onOpenChange: onDeleteModalChange } = useDisclosure();
     const [selectedTransaction, setSelectedTransaction] = useState<Expense | null>(null);
 
     const getTransactionsData = async () => {
         const transactionsData = await getExpenses(currentTransactionsPage, 7, transactionTypeSelected);
-        if(currentTransactionsPage == 1) {
+        if (currentTransactionsPage == 1) {
             setTransactions(transactionsData);
         } else {
             setTransactions([...transactions, ...transactionsData]);
@@ -64,7 +64,7 @@ export default function TransactionsList() {
 
     useEffect(() => {
         getTransactionsData();
-    },  []);
+    }, []);
 
     useEffect(() => {
         setCurrentTransactionsPage(1);
@@ -87,20 +87,20 @@ export default function TransactionsList() {
         )
     }
     return (
-        
+
         <Card className="border-none rounded-3xl shadow-md overflow-hidden bg-gradient-to-br from-white to-[#f9faff] p-6 relative">
             <div className="absolute top-0 right-0 w-64 h-64 bg-gradient-to-br from-[#5a6bff]/5 to-transparent rounded-full -translate-y-32 translate-x-32"></div>
             <div className="absolute bottom-0 left-0 w-32 h-32 bg-gradient-to-tr from-[#5a6bff]/5 to-transparent rounded-full translate-y-16 -translate-x-16"></div>
             <div className="flex items-center justify-between content-center">
                 <h2 className="text-xl font-extralight">Ultimas transacciones </h2>
                 <ButtonGroup variant="flat" size="sm">
-                    <Button 
-                        color={getColorByTransactionType(TransactionType.Outcome)} 
+                    <Button
+                        color={getColorByTransactionType(TransactionType.Outcome)}
                         onPress={() => setTransactionTypeSelected(TransactionType.Outcome)}
                     >
                         Gastos
                     </Button>
-                    <Button 
+                    <Button
                         color={getColorByTransactionType(TransactionType.Income)}
                         onPress={() => setTransactionTypeSelected(TransactionType.Income)}
                     >
@@ -114,14 +114,14 @@ export default function TransactionsList() {
                         key={transaction.id}
                     >
                         <DropdownTrigger className="w-full">
-                            <div 
-                                key={transaction.id} 
+                            <div
+                                key={transaction.id}
                                 className="group flex items-center justify-between p-2 rounded-xl bg-gradient-to-r from-white to-[#f9faff] border border-[#f0f4ff] hover:shadow-md hover:border-[#e4e9ff] transition-all duration-200"
                             >
                                 <div className="flex items-center gap-2">
                                     <div className={`w-6 h-6 rounded-full bg-gradient-to-br ${transaction.transactionType === TransactionType.Income ? 'from-[#2dd4bf] to-[#34d399]' : 'from-[#f97066] to-[#fb7185]'} flex items-center justify-center transform transition-transform duration-300 group-hover:scale-110`}>
                                         {
-                                            transaction.transactionType === TransactionType.Income ? 
+                                            transaction.transactionType === TransactionType.Income ?
                                                 <FaAngleDoubleUp className="h-3 w-3 text-white" /> :
                                                 <FaAngleDoubleDown className="h-3 w-3 text-white" />
                                         }
@@ -136,16 +136,15 @@ export default function TransactionsList() {
                                     </div>
                                 </div>
                                 <div className="flex flex-col items-end">
-                                    <span 
-                                        className={`text-base font-medium ${
-                                            transaction.transactionType === TransactionType.Income
-                                                ? 'bg-gradient-to-r from-[#2dd4bf] to-[#34d399] bg-clip-text text-transparent' 
+                                    <span
+                                        className={`text-base font-medium ${transaction.transactionType === TransactionType.Income
+                                                ? 'bg-gradient-to-r from-[#2dd4bf] to-[#34d399] bg-clip-text text-transparent'
                                                 : 'bg-gradient-to-r from-[#f97066] to-[#fb7185] bg-clip-text text-transparent'
-                                        }`}
+                                            }`}
                                     >
                                         {transaction.transactionType === TransactionType.Income ? '+' : '-'} {formatCurrency(transaction.value || 0)}
                                     </span>
-                                    <div 
+                                    <div
                                         className="text-[10px] font-light text-white px-2 py-0.5 rounded-full bg-[#121432]/70"
                                     >
                                         {transaction.category?.parent}
@@ -153,7 +152,7 @@ export default function TransactionsList() {
                                 </div>
                             </div>
                         </DropdownTrigger>
-                        <DropdownMenu aria-label="Transaction options" className="w-full" onAction={(action) => {executeTransactionAction(action, transaction)}}>
+                        <DropdownMenu aria-label="Transaction options" className="w-full" onAction={(action) => { executeTransactionAction(action, transaction) }}>
                             <DropdownItem
                                 key="delete"
                                 startContent={<FaTimesCircle className="text-danger" />}
@@ -165,17 +164,17 @@ export default function TransactionsList() {
                     </Dropdown>
                 ))}
             </div>
-            <ConfirmModal 
-                isOpen={isDeleteModalOpen} 
+            <ConfirmModal
+                isOpen={isDeleteModalOpen}
                 onOpenChange={onDeleteModalChange}
-                title="Eliminar transacción" 
-                message="¿Estás seguro de que quieres eliminar esta transacción?" 
+                title="Eliminar transacción"
+                message="¿Estás seguro de que quieres eliminar esta transacción?"
                 onConfirm={onConfirmDeleteTransaction}
             />
             <div className="mt-4 flex items-center justify-between relative">
-                <Button 
-                    variant="light" 
-                    size="lg" 
+                <Button
+                    variant="light"
+                    size="lg"
                     className="text-xs font-normal flex items-center w-full"
                     onPress={onLoadMoreTransactions}
                 >
